3b654f4da4eb334fc81b142e56ee6a3d2265c68a
Djima OTCHAMBO <otchambodjima@gmail.com>
2026-08-10 22:37:31 +0100

    Add TEAMS constant with football club names

---
+++ js/data.js
@@ -0,0 +1,17 @@
+const TEAMS = [
+    "MA'OR FC",
+    "Ichokan FC",
+    "Koussoukpa FC",
+    "New King FC",
+    "Club Olympique FC",
+    "Providence FC",
+    "AS Police FC",
+    "Ayetedjou FC",
+    "Agadjaligbo FC",
+    "Club Les Amis FC",
+    "Sport Foot FC",
+    "Réveil FC",
+    "Hlagba Zakpo FC",
+    "Zalimey FC",
+    "Zougame FC"
+];
