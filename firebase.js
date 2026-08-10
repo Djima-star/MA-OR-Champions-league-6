e54185ee63dc874ad789bcdddc813e4d0e025330
Djima OTCHAMBO <otchambodjima@gmail.com>
2026-08-11 00:30:29 +0100

    Migrate from Realtime Database to Firestore

--- firebase.js
+++ firebase.js
@@ -1,46 +1,85 @@
-// Firebase SDK
+// ================================
+// FIREBASE - MA'OR CHAMPIONS LEAGUE
+// ================================
+
 import { initializeApp } from
 "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import {
-    getDatabase,
-    ref,
-    push,
-    set,
-    onValue,
-    update,
-    remove
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    query,
+    orderBy,
+    onSnapshot,
+    doc,
+    updateDoc,
+    deleteDoc
 } from
-"https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+"https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 
 // Configuration Firebase
+
 const firebaseConfig = {
+
     apiKey: "AIzaSyBcARMNpu2Hh92lGP68icBTZV2a_XGYrfc",
-    authDomain: "ma-or-champions-league.firebaseapp.com",
-    projectId: "ma-or-champions-league",
-    storageBucket: "ma-or-champions-league.firebasestorage.app",
-    messagingSenderId: "524486036430",
-    appId: "1:524486036430:web:b49ee45b8062bae5d3399f",
-    measurementId: "G-LFVG51W3V4"
+
+    authDomain:
+        "ma-or-champions-league.firebaseapp.com",
+
+    projectId:
+        "ma-or-champions-league",
+
+    storageBucket:
+        "ma-or-champions-league.firebasestorage.app",
+
+    messagingSenderId:
+        "524486036430",
+
+    appId:
+        "1:524486036430:web:b49ee45b8062bae5d3399f",
+
+    measurementId:
+        "G-LFVG51W3V4"
 };
 
 
-// Initialisation Firebase
-const app = initializeApp(firebaseConfig);
+// Initialiser Firebase
+
+const app =
+    initializeApp(firebaseConfig);
 
 
-// Connexion Realtime Database
-const database = getDatabase(app);
+// Initialiser Firestore
+
+const db =
+    getFirestore(app);
 
 
-// Exportation
+// Exporter
+
 export {
-    database,
-    ref,
-    push,
-    set,
-    onValue,
-    update,
-    remove
+
+    db,
+
+    collection,
+
+    addDoc,
+
+    getDocs,
+
+    query,
+
+    orderBy,
+
+    onSnapshot,
+
+    doc,
+
+    updateDoc,
+
+    deleteDoc
+
 };
