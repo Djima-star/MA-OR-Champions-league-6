01edae28646873fcca51c0d80ad9e3181bc8555b
Djima OTCHAMBO <otchambodjima@gmail.com>
2026-08-11 15:51:04 +0100

    Delete js/firebase -config.js

--- js/firebase -config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBcARMNpu2Hh92lGP68icBTZV2a_XGYrfc",
-    authDomain: "ma-or-champions-league.firebaseapp.com",
-    projectId: "ma-or-champions-league",
-    storageBucket: "ma-or-champions-league.firebasestorage.app",
-    messagingSenderId: "524486036430",
-    appId: "1:524486036430:web:b49ee45b8062bae5d3399f",
-    measurementId: "G-LFVG51W3V4"
-};
